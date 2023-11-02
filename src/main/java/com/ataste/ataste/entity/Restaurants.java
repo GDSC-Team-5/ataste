7e879a063a1ae047f1ce96c5b36b636c7a2f182b
jjindol <andy15948@naver.com>
2023-11-02 18:17:13 +0900

    first commit

--- src/main/java/com/ataste/ataste/entity/Restaurants.java
+++ src/main/java/com/ataste/ataste/entity/Restaurants.java
@@ -10,18 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-@NoArgsConstructor
-@Setter
-@Getter
+
 public class Restaurants {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long restaurant_id;
-    private String name;
-    private String category; // # {카테고리}
-    private String comment;
-    private int likes;
-
+    
 }
