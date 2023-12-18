3029d9e93d19e45b3805802bd939dd0c48cf6473
joochaeyeon <inha031913@gmail.com>
2023-12-18 09:50:49 +0900

    fixed front local

--- src/main/java/com/ataste/ataste/config/WebConfig.java
+++ src/main/java/com/ataste/ataste/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://104.198.104.70:3000") // http://localhost:3000
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS", "DELETE");
 //                .allowCredentials(true);
     }
